0e26a94ffd36e53428175af8f28d2e227750e5e8
Nguyễn Hà Kiên <135228733+kinatraa@users.noreply.github.com>
2025-11-25 19:01:15 +0700

    add: managers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : SingletonDestroy<UIManager>
+public class GameManager : Singleton<GameManager>
 {
     
 }
