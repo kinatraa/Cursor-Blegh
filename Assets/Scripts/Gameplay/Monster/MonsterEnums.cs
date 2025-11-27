bc4c2b65577635b3df024882d87171038aa2174d
Duc Anh <nguyenanhduc2938@gmail.com>
2025-11-27 12:56:21 +0700

    monster enums

---
+++ Assets/Scripts/Gameplay/Monster/MonsterEnums.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public enum MonsterName
+{
+    None = 0,
+    SlimeNguyenThuy=1, 
+    QuaiVatLua=2,    
+    YeuTinhThienXa=3, 
+    YeuTinhSatThu=4, 
+    VuaXuong=5,
+    NamMau=6,
+    NamDoc=7,
+    MatTroi=8,            
+    LoiThu=9,             
+    SlimeDoc=10,
+    SlimeLua=11,
+    PhapSuULinh=12,
+    LongThu=13,
+    VuaQuy=14
+}
+public enum AttackRange
+{
+    Melee=0,  
+    Ranged=11  
+}
+public enum MonsterSize
+{
+    Small=0,  
+    Medium=1,
+    Large=2 
+}
