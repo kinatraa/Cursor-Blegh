eee3b31f28f4c7cf5b520db7ed736528475ed6b5
hakien <135228733+kinatraa@users.noreply.github.com>
2025-11-27 15:31:23 +0700

    add: spawn monster

--- Assets/Scripts/Gameplay/Monster/MonsterController.cs
+++ Assets/Scripts/Gameplay/Monster/MonsterController.cs
@@ -14,12 +14,40 @@ public class MonsterController : MonoBehaviour
     {
         foreach (var monsterPrefab in monsterPrefabs)
         {
-            _monsterDict.Add(monsterPrefab.data.type, monsterPrefab);
+            _monsterDict.TryAdd(monsterPrefab.data.type, monsterPrefab);
         }
     }
 
     public void SpawnMonster(MonsterType type, Vector3 position)
     {
+        if (!_monsterDict.ContainsKey(type))
+        {
+            Debug.LogError($"Monster {type} not found");
+            return;
+        }
+        
+        var prefab = _monsterDict[type];
+        Instantiate(prefab, position, Quaternion.identity, transform);
+    }
+    
+    public void SpawnMonster(string typeStr, Vector3 position)
+    {
+        MonsterType type = MonsterType.NONE;
+        foreach (MonsterType t in System.Enum.GetValues(typeof(MonsterType)))
+        {
+            if (t.ToString() == typeStr)
+            {
+                type = t;
+                break;
+            }
+        }
+
+        if (!_monsterDict.ContainsKey(type))
+        {
+            Debug.LogError($"Monster {typeStr} not found");
+            return;
+        }
+        
         var prefab = _monsterDict[type];
         Instantiate(prefab, position, Quaternion.identity, transform);
     }
